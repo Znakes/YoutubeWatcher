4bdbe2c9976ceb86f124431f640b3c762b6afd23
Grigoriev Igor <j-nets@yandex.ru>
2015-09-01 12:18:13 +0300

    +replaced ResourceStream

---
+++ YoutubeWatcher/Resources/ResourceStream.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using YuInfoRetriever.Authorization;
+
+namespace YoutubeWatcher
+{
+    internal class ResourceStream : IAuthProvider
+    {
+        public string ResourcePath { get; private set; }
+
+        public void SetParams(object param)
+        {
+            if (param != null && !string.IsNullOrEmpty(param.ToString()))
+            {
+                ResourcePath = param.ToString();
+                return;
+            }
+
+            throw new NullReferenceException();
+        }
+
+        public Stream GetAuthDataStream()
+        {
+            if (string.IsNullOrEmpty(ResourcePath))
+                throw new NullReferenceException("ResourcePath");
+
+            //@"Resources/client_secrets.json"
+            var sri = Application.GetResourceStream(new Uri(ResourcePath, UriKind.Relative));
+            if (sri != null)
+            {
+                return sri.Stream;
+            }
+
+            throw new NullReferenceException("sri");
+        }
+    }
+}
